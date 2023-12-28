01e833fb47dccf6ed5db23a054cb3223b0f1b574
Desi <83933926+debreeze20@users.noreply.github.com>
2023-12-28 20:27:21 +0700

    updated

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -10,9 +10,36 @@
 <a href="{{ url('/cars/create') }}">Tambah Mobil</a>
 
 <ul>
-    @foreach ($cars as $car)
-        <li>{{ $car->brand }} - {{ $car->model }} ({{ $car->plate_number }})</li>
-    @endforeach
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h2>Daftar Mobil</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Merek</th>
+                    <th>Model</th>
+                    <th>Nomor Plat</th>
+                    <th>Tarif Sewa per Hari</th>
+                    <!-- Tambahkan kolom lain sesuai kebutuhan -->
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($cars as $car)
+                    <tr>
+                        <td>{{ $car->brand }}</td>
+                        <td>{{ $car->model }}</td>
+                        <td>{{ $car->plate_number }}</td>
+                        <td>{{ $car->rental_rate }}</td>
+                        <!-- Tambahkan kolom lain sesuai kebutuhan -->
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
+
 </ul>
 
 </body>
